a9559623d6692656ffe92f10d8902b261af5d27e
javajigi <javajigi@gmail.com>
2016-06-27 15:57:40 +0900

    Board 삭제에 대한 다양한 예제 추가함.

--- src/main/java/next/web/BoardController.java
+++ src/main/java/next/web/BoardController.java
@@ -3,6 +3,7 @@ package next.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,5 +21,15 @@ public class BoardController {
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public ResponseEntity<Board> create(@RequestBody Board board) {
 		return new ResponseEntity<Board>(boardRepository.save(board), HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Board> delete(@PathVariable Long id) {
+		Board board = boardRepository.findOne(id);
+		if (board == null) {
+			return new ResponseEntity<Board>(HttpStatus.NOT_FOUND);
+		}
+		boardRepository.delete(board);
+		return new ResponseEntity<Board>(HttpStatus.NO_CONTENT);
 	}
 }
--- src/test/java/next/web/BoardApiTest.java
+++ src/test/java/next/web/BoardApiTest.java
@@ -8,18 +8,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import core.test.WebIntegrationTest;
 import next.domain.Board;
+import next.domain.BoardRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class BoardApiTest extends WebIntegrationTest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(BoardApiTest.class);
 	
+	@Autowired
+	private BoardRepository boardRepository;
 	private RestTemplate template;
 	
 	@Before
@@ -40,5 +46,26 @@ public class BoardApiTest extends WebIntegrationTest {
 		ResponseEntity<Board> responseEntity = template.postForEntity(baseUrl() + "/boards", board, Board.class);
 		assertThat(responseEntity.getStatusCode(), is(HttpStatus.CREATED));
 		LOGGER.debug("create board : {}", responseEntity.getBody());
+	}
+	
+	@Test
+	public void delete() throws Exception {
+		Board board = boardRepository.save(new Board("deleteBoard"));
+		template.delete(baseUrl() + "/boards/" + board.getId());
+		assertThat(boardRepository.findOne(board.getId()), is(nullValue()));
+	}
+	
+	@Test(expected = HttpClientErrorException.class)
+	public void delete404NotFound() throws Exception {
+		String notFoundUrl = baseUrl() + "/boards/0";
+		template.exchange(notFoundUrl, HttpMethod.DELETE, null, Board.class);
+	}
+	
+	@Test
+	public void deleteExchange() throws Exception {
+		Board board = boardRepository.save(new Board("deleteBoard"));
+		String noContentUrl = baseUrl() + "/boards/" + board.getId();
+		ResponseEntity<Board> responseEntity = template.exchange(noContentUrl, HttpMethod.DELETE, null, Board.class);
+		assertThat(responseEntity.getStatusCode(), is(HttpStatus.NO_CONTENT));
 	}
 }
