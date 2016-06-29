7af58c7afb6d2aa122faafde38f91b8678136ac1
javajigi <javajigi@gmail.com>
2016-06-29 12:57:04 +0900

    Board 목록과 상세보기 페이지 html까지 연동 완료함.

--- src/main/java/next/Application.java
+++ src/main/java/next/Application.java
@@ -1,37 +1,31 @@
 package next;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-import next.domain.Card;
-import next.domain.CardRepository;
+import next.domain.Board;
+import next.domain.BoardRepository;
+import next.domain.Deck;
+import next.domain.DeckRepository;
 
 @SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
 public class Application {
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 	
 	@Bean
-	public CommandLineRunner demo(CardRepository repository) {
+	public CommandLineRunner demo(BoardRepository boardRepository, DeckRepository deckRepository) {
 		return (args) -> {
-			repository.save(new Card("title1", "description1"));
-			repository.save(new Card("title2", "description2"));
-
-			// fetch all customers
-			log.info("Cards found with findAll():");
-			log.info("-------------------------------");
-			for (Card customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-            log.info("");
+			Board board1 = boardRepository.save(new Board("my first board"));
+			deckRepository.save(new Deck(board1, "my first list1"));
+			deckRepository.save(new Deck(board1, "my first list2"));
+			deckRepository.save(new Deck(board1, "my first list3"));
+			Board board2= boardRepository.save(new Board("my second board"));
+			deckRepository.save(new Deck(board2, "my second list1"));
 		};
 	}
 }
--- src/main/java/next/web/TrelloController.java
+++ src/main/java/next/web/TrelloController.java
@@ -1,15 +1,23 @@
 package next.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import next.domain.BoardRepository;
+
 @Controller
 @RequestMapping(method = RequestMethod.GET)
 public class TrelloController {
+	@Autowired
+	private BoardRepository boardRepository;
+	
 	@RequestMapping("/")
-    public String boards() {
+    public String boards(Model model) {
+		model.addAttribute("boards", boardRepository.findAll());
         return "boards";
     }
 	
@@ -19,7 +27,8 @@ public class TrelloController {
     }
 	
 	@RequestMapping("/b/{id}")
-    public String board(@PathVariable Long id) {
+    public String board(@PathVariable Long id, Model model) {
+		model.addAttribute("board", boardRepository.findOne(id));
         return "board";
     }
 }
