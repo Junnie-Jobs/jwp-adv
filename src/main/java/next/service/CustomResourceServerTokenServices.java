10424990892bdf70cb629b70a22a486515852559
javajigi <javajigi@gmail.com>
2016-07-14 14:50:30 +0900

    OAuth2RestTemplate 대신 RestTemplate을 사용하도록 수정함.

--- src/main/java/next/service/CustomResourceServerTokenServices.java
+++ src/main/java/next/service/CustomResourceServerTokenServices.java
@@ -1,48 +1,43 @@
 package next.service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.BaseOAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.web.client.RestTemplate;
 
 import next.domain.user.GitHubUser;
 import next.domain.user.GitHubUserRepository;
 
 public class CustomResourceServerTokenServices implements ResourceServerTokenServices {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomResourceServerTokenServices.class);
-	
+
 	@Autowired
 	private GitHubUserRepository githubUserRepository;
-	
+
 	private final String userInfoEndpointUrl;
 
 	private final String clientId;
-
-	private OAuth2RestOperations restTemplate;
-
-	private String tokenType = DefaultOAuth2AccessToken.BEARER_TYPE;
 
 	public CustomResourceServerTokenServices(String userInfoEndpointUrl, String clientId) {
 		this.userInfoEndpointUrl = userInfoEndpointUrl;
 		this.clientId = clientId;
 	}
-	
+
 	@Override
 	public OAuth2Authentication loadAuthentication(String accessToken)
 			throws AuthenticationException, InvalidTokenException {
@@ -51,51 +46,36 @@ public class CustomResourceServerTokenServices implements ResourceServerTokenSer
 		if (githubUser != null) {
 			return extractAuthentication(githubUser);
 		}
-		
-		Map<String, Object> map = getMap(this.userInfoEndpointUrl, accessToken);
-		LOGGER.debug("UserInfo : {}", map);
-		if (map.containsKey("error")) {
-			LOGGER.debug("userinfo returned error: " + map.get("error"));
+
+		Map<String, Object> userInfo = getUserInfo(this.userInfoEndpointUrl, accessToken);
+		LOGGER.debug("UserInfo : {}", userInfo);
+		if (userInfo.containsKey("error")) {
+			LOGGER.debug("userinfo returned error: " + userInfo.get("error"));
 			throw new InvalidTokenException(accessToken);
 		}
-		githubUser = new GitHubUser(map.get("login") + "", map.get("email") + "", map.get("name") + "", accessToken);
+		githubUser = new GitHubUser(userInfo.get("login") + "", userInfo.get("email") + "", userInfo.get("name") + "", accessToken);
 		githubUserRepository.save(githubUser);
 		return extractAuthentication(githubUser);
 	}
-	
+
 	private OAuth2Authentication extractAuthentication(GitHubUser githubUser) {
-		List<GrantedAuthority> authorities = AuthorityUtils
-				.commaSeparatedStringToAuthorityList("ROLE_USER");
-		OAuth2Request request = new OAuth2Request(null, this.clientId, null, true, null,
-				null, null, null, null);
-		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-				githubUser.getUserId(), "N/A", authorities);
+		List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+		OAuth2Request request = new OAuth2Request(null, this.clientId, null, true, null, null, null, null, null);
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(githubUser.getUserId(),
+				"N/A", authorities);
 		token.setDetails(githubUser);
 		return new OAuth2Authentication(request, token);
 	}
-	
+
 	@SuppressWarnings({ "unchecked" })
-	private Map<String, Object> getMap(String path, String accessToken) {
-		try {
-			OAuth2RestOperations restTemplate = this.restTemplate;
-			if (restTemplate == null) {
-				BaseOAuth2ProtectedResourceDetails resource = new BaseOAuth2ProtectedResourceDetails();
-				resource.setClientId(this.clientId);
-				restTemplate = new OAuth2RestTemplate(resource);
-			}
-			DefaultOAuth2AccessToken token = new DefaultOAuth2AccessToken(accessToken);
-			token.setTokenType(this.tokenType);
-			restTemplate.getOAuth2ClientContext().setAccessToken(token);
-			return restTemplate.getForEntity(path, Map.class).getBody();
-		}
-		catch (Exception ex) {
-			LOGGER.info("Could not fetch user details: " + ex.getClass() + ", "
-					+ ex.getMessage());
-			return Collections.<String, Object> singletonMap("error",
-					"Could not fetch user details");
-		}
+	private Map<String, Object> getUserInfo(String path, String accessToken) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + accessToken);
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.exchange(path, HttpMethod.GET, entity, Map.class).getBody();
 	}
-	
+
 	@Override
 	public OAuth2AccessToken readAccessToken(String accessToken) {
 		throw new UnsupportedOperationException("Not supported: read access token");
