10ef4d4da756797c1e35772c319b6c5fa38f8715
javajigi <javajigi@gmail.com>
2016-07-19 20:51:21 +0900

    UserMixin 제거해서 컴파일 에러 발생하는 부분 수정함.

--- src/main/java/next/config/WebMvcConfig.java
+++ src/main/java/next/config/WebMvcConfig.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import core.web.argumentresolver.LoginUserHandlerMethodArgumentResolver;
-import next.domain.user.User;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -21,12 +19,5 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Bean
     public LoginUserHandlerMethodArgumentResolver loginUserHandlerMethodArgumentResolver() {
     	return new LoginUserHandlerMethodArgumentResolver();
-    }
-    
-    @Bean
-    public Jackson2ObjectMapperBuilder jacksonBuilder() {
-    	Jackson2ObjectMapperBuilder b = new Jackson2ObjectMapperBuilder();
-    	b.mixIn(User.class, UserMixin.class);
-    	return b;
     }
 }
