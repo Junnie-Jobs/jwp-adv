a1218c93250f1043b63c66a4565760a48c800735
javajigi <javajigi@gmail.com>
2016-08-08 12:58:39 +0900

    서버 정보를 확인할 수 있는 api 추가함.

---
+++ src/main/java/next/web/ApiTrelloController.java
@@ -0,0 +1,21 @@
+package next.web;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Maps;
+
+@RestController
+public class ApiTrelloController {
+	@RequestMapping("/servers/info")
+	public Map<String, Object> info(HttpServletRequest request) {
+		Map<String, Object> infos = Maps.newHashMap();
+		infos.put("ServerName", request.getServerName());
+		infos.put("Port", request.getServerPort());
+		return infos;
+	}
+}
